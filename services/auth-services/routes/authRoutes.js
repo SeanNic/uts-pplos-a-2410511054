b5918e4987468f4152e8b2b8805a39871d486bb1
SeanNic <SeanNic@users.noreply.github.com>
2026-05-01 10:20:52 +0700

    feat: add jwt middleware and protected route

--- services/auth-services/routes/authRoutes.js
+++ services/auth-services/routes/authRoutes.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const { register, login } = require("../controllers/authController");
+const verifyToken = require("../middlewares/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/profile", verifyToken, (req, res) => {
+  res.status(200).json({
+    message: "Protected route success",
+    user: req.user,
+  });
+});
 
 module.exports = router;
--- services/auth-services/server.js
+++ services/auth-services/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
+
 app.get("/", (req, res) => {
-  res.json({
-    message: "Auth Service Running",
-  });
+  res.json({ message: "Auth Service Running" });
 });
 
 const PORT = process.env.PORT || 3001;
@@ -18,7 +20,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Auth service running on port ${PORT}`);
 });
-
-const authRoutes = require("./routes/authRoutes");
-
-app.use("/auth", authRoutes);
